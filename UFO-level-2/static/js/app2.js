86cd386ff533ff411fa09a221cd40064160d64fa
SergioGL <67619423+SergioGL8@users.noreply.github.com>
2020-11-02 00:26:13 -0600

    Level2

--- UFO-level-2/static/js/app2.js
+++ UFO-level-2/static/js/app2.js
@@ -4,16 +4,11 @@
 // from data.js
 var tableData = data;
 
-// function to display all
-function displayData(Sightings) {
-
-  // select tbody
+// function to display UFO sightings
+function tableDisplay(ufoSightings) {
   var tbody = d3.select("tbody");
-  Sightings.forEach((ufoRecord) => {
+  ufoSightings.forEach((ufoRecord) => {
     var row = tbody.append("tr");
-    
-    // using Object.entries to get key, value data inside of the table
-    // and loop through them to add to the table in html
     Object.entries(ufoRecord).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.html(value);
@@ -27,48 +22,48 @@ function deleteTbody() {
     .selectAll("tr").remove()
     .selectAll("td").remove();
 };
-
-// display all 
+  
+// initial display of all UFO sightings
 console.log(tableData);
-displayData(tableData);
+tableDisplay(tableData);
 
 // 'Filter Table' button
 var button = d3.select("#filter-btn");
 
-// filter data with the properties that the user inputs and display
+// filter the database
 button.on("click", function(event) {
+  
   d3.event.preventDefault();
   deleteTbody();
-
+  
   var filteredData = tableData;
-  var input = document.getElementsByClassName("form-control");
-
-  // iterate through the input fields
-  for (var i = 0; i < input.length; i++) {
-
-    var idName = input[i].id;
-    var field = d3.select("#" + idName).property("value");
-    
-    // treat empty or space-only fields as a search for ALL for that field
-    if (field.trim() !== "") {
-      var filteredData = filteredData.filter(ufoSighting =>
-
-        // match as case insensitive
-        ufoSighting[idName].toUpperCase().trim() ===
-        field.toUpperCase().trim());
-    };
+  var inputId = document.getElementsByClassName("form-control");
+  
+  // iterate through all the input fields
+  for (var i = 0; i < inputId.length; i++) {
+	
+	var idName = inputId[i].id;
+	var field = d3.select("#" + idName).property("value");
+	
+	// treat empty or space-only fields as a search for ALL for that field
+	if (field.trim() !== "") {
+	  var filteredData = filteredData.filter(ufoSighting =>
+	    // match as case insensitive
+		ufoSighting[idName].toUpperCase().trim() ===
+		field.toUpperCase().trim());
+	};
   };
-
-  // display a message if no records found
+ 
+  // display message if no records found
   if (filteredData.length == 0) {
     d3.select("tbody")
       .append("tr")
       .append("td")
         .attr("colspan", 7)
-        .html("<h3>No Records Found</h3>");
+        .html("<h4>No Records Found</h4>");
   };
-
-  // print the value and display the new table that was input
+  
+  // display the database
   console.log(filteredData);
-  displayData(filteredData);
+  tableDisplay(filteredData);
 });
